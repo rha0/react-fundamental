38eef53fa0c7dd09acadf66dfd5666f90003dc7d
many_u <39545238+rha0@users.noreply.github.com>
2024-06-03 15:21:02 +0900

    유효성 검사 로직
    -폼 제출시, 초점 잃을 때, 매 번 키보드 칠 때 기능 구현

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,66 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState(" ");
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const [enteredNameIsTouched, setEnteredNameIsTouched] = useState(false);
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+
+    setEnteredNameIsTouched(true);
+
+    if (event.target.value.trim() === "") {
+      setEnteredNameIsValid(false);
+    } else setEnteredNameIsValid(true);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    setEnteredNameIsTouched(true);
+
+    // enteredName이 빈 값인 경우 제출이 안 되게 수정
+    // trim()을 적용했을 때 " abc " -> "abc"
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
+      return;
+    }
+    setEnteredNameIsValid(true);
+    setEnteredName("");
+  };
+
+  const nameInputBlurHandler = (event) => {
+    setEnteredNameIsTouched(true);
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
+      return;
+    }
+  };
+
+  // 에러 메시지를 보여주는 경우, true -> 메시지 노출
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameIsTouched;
+
+  const nameInputClasses = nameInputIsInvalid
+    ? "form-control invalid"
+    : "form-control";
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>당신의 이름은?</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmitHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor="name">당신의 이름은?</label>
+        <input
+          type="text"
+          id="name"
+          ref={nameInputRef}
+          value={enteredName}
+          onChange={nameInputChangeHandler}
+          onBlur={nameInputBlurHandler}
+        />
+        {nameInputIsInvalid && (
+          <p className="error-text">이름값은 빈 값이 아니어야 합니다.</p>
+        )}
       </div>
       <div className="form-actions">
         <button>제출하기</button>

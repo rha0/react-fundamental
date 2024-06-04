d18b6840e1fb65f0d70b8ac7739b145837568058
many_u <39545238+rha0@users.noreply.github.com>
2024-06-04 13:17:39 +0900

    숫자 카운터 증가, 감소 버튼 추가
    useSelector, useDispatch를 활용하여 숫자 카운터 증가 버튼 구현

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,27 @@
-import classes from './Counter.module.css';
+import { useDispatch, useSelector } from "react-redux";
+import classes from "./Counter.module.css";
 
 const Counter = () => {
   const toggleCounterHandler = () => {};
 
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
+
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- 카운터 값 --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>토글 카운터</button>
+      <button onClick={incrementHandler}>숫자 증가</button>
+      <button onClick={decrementHandler}>숫자 감소</button>
     </main>
   );
 };

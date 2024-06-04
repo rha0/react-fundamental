a80a51cf23b32391f183b58f3530b181551eb586
many_u <39545238+rha0@users.noreply.github.com>
2024-06-04 13:56:36 +0900

    payload 전달
    -payload 값 전달
    -toggle 카운터 동작 구현

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,13 +2,21 @@ import { useDispatch, useSelector } from "react-redux";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
 
   const counter = useSelector((state) => state.counter);
+  const showCounter = useSelector((state) => state.showCounter);
   const dispatch = useDispatch();
 
   const incrementHandler = () => {
     dispatch({ type: "increment" });
+  };
+
+  // payload 전달
+  const increase10Handler = () => {
+    dispatch({ type: "increase", amount: 10 });
   };
 
   const decrementHandler = () => {
@@ -18,9 +26,10 @@ const Counter = () => {
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
       <button onClick={toggleCounterHandler}>토글 카운터</button>
       <button onClick={incrementHandler}>숫자 증가</button>
+      <button onClick={increase10Handler}>숫자 10 증가</button>
       <button onClick={decrementHandler}>숫자 감소</button>
     </main>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,36 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const counterReducer = (state = { counter: 0, showCounter: true }, action) => {
   if (action.type === "increment") {
     return {
+      // 바뀌는 값들만 바꿔주고, 나머지 값들은 ...state 처리해서 기존 state 값을 가져오기
+      ...state,
       counter: state.counter + 1,
+      //   showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === "increase") {
+    return {
+      ...state,
+      counter: state.counter + action.amount,
+      //   showCounter: state.showCounter,
     };
   }
 
   if (action.type === "decrement") {
     return {
+      ...state,
       counter: state.counter - 1,
+      //   showCounter: state.showCounter,
+    };
+  }
+
+  // toggle counter
+  if (action.type === "toggle") {
+    return {
+      ...state,
+      showCounter: !state.showCounter,
     };
   }
 

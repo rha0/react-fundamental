ed5c4bed180e0994b8200a1796f4d5cef285e6b3
many_u <39545238+rha0@users.noreply.github.com>
2024-06-04 16:11:31 +0900

    redux toolKit
    -컴포넌트에 auth 여부 적용
    -로그인 시 auth 적용, 프로필 노출
    -로그아웃 시 auth 해제, 로그인 창 노출

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,6 +1,7 @@
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from "react-redux";
 
-import classes from './Counter.module.css';
+import classes from "./Counter.module.css";
+import { counterActions } from "../store";
 
 const Counter = () => {
   const dispatch = useDispatch();
@@ -8,19 +9,19 @@ const Counter = () => {
   const show = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
-    dispatch({ type: 'increment' });
+    dispatch(counterActions.increment());
   };
 
   const increaseHandler = () => {
-    dispatch({ type: 'increase', amount: 10 });
+    dispatch(counterActions.increase(10));
   };
 
   const decrementHandler = () => {
-    dispatch({ type: 'decrement' });
+    dispatch(counterActions.decrement());
   };
 
   const toggleCounterHandler = () => {
-    dispatch({ type: 'toggle' });
+    dispatch(counterActions.toggleCounter());
   };
 
   return (
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,30 @@
-import classes from './Header.module.css';
+import { authActions } from "../store";
+import classes from "./Header.module.css";
+import { useDispatch, useSelector } from "react-redux";
 
 const Header = () => {
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+  const dispatch = useDispatch();
+
+  const logoutHandler = () => {
+    dispatch(authActions.logout());
+  };
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href='/'>내 상품들</a>
-          </li>
-          <li>
-            <button>로그아웃</button>
-          </li>
-        </ul>
-      </nav>
+      {isAuth && (
+        <nav>
+          <ul>
+            <li>
+              <a href="/">내 상품들</a>
+            </li>
+            <li>
+              <button onClick={logoutHandler}>로그아웃</button>
+            </li>
+          </ul>
+        </nav>
+      )}
     </header>
   );
 };

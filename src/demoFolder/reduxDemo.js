0952e412a95581a50c274bcffe5c7db16c400ba8
many_u <39545238+rha0@users.noreply.github.com>
2024-06-04 11:00:16 +0900

    redux 데모 만들기

---
+++ src/demoFolder/reduxDemo.js
@@ -0,0 +1,28 @@
+const redux = require("redux");
+
+const conunterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return {
+      conuter: state.counter + 1,
+    };
+  }
+
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  return state;
+};
+
+const store = redux.createStore(conunterReducer);
+
+const counterSubscriber = () => {
+  const latestState = store.getState();
+  console.log(latestState);
+};
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({ type: "increment" });
+store.dispatch({ type: "decrement" });

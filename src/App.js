78fee192ca16f7489823f9564c6db96082b08fdf
many_u <39545238+rha0@users.noreply.github.com>
2024-06-04 19:10:07 +0900

    리덕스 비동기 처리
    -장바구니 아이템 추가, 삭제
    -Firebase 데이터베이스에서 데이터 불러오기, 저장
    -로직 비동기 처리

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { useEffect } from "react";
+import { fetchCartData, sendCartData } from "./store/cart-slice";
+
+let isInitial = true;
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    dispatch(sendCartData(cart));
+  }, [cart, dispatch]);
+
+  // 컴포넌트 안에서 side-effect 로직 및 비동기 로직을 처리하기 e.g. useEffect( )
+  // useEffect(() => {
+  //   fetch("https://react-test-5cf3a-default-rtdb.firebaseio.com/cart.json", {
+  //     method: "PUT",
+  //     body: JSON.stringify(cart),
+  //   });
+  // }, [cart]);
 
   return (
     <Layout>
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,14 +1,95 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
+
+export const fetchCartData = () => {
+  return async (dispatch) => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://react-test-5cf3a-default-rtdb.firebaseio.com/cart.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("실패");
+      }
+
+      const data = await response.json();
+
+      return data;
+    };
+
+    try {
+      const cartData = await fetchData();
+      dispatch(cartActions.replaceCart(cartData));
+    } catch (error) {
+      //에러 처리
+    }
+  };
+};
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://react-test-5cf3a-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("실패");
+      }
+    };
+
+    try {
+      await sendRequest();
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+};
 
 const cartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState: {
     items: [],
     totalQuantity: 0,
   },
   reducers: {
-    addItemToCart(state, action) { /*TODO- 여기에 코드를 작성해 주세요*/},
-    removeItemFromCart(state, action) { /*TODO- 여기에 코드를 작성해 주세요*/},
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
+    addItemToCart(state, action) {
+      /*TODO- 여기에 코드를 작성해 주세요*/
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
+      if (!existingItem) {
+        state.items.push({
+          id: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title,
+        });
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+      }
+    },
+    removeItemFromCart(state, action) {
+      /*TODO- 여기에 코드를 작성해 주세요*/
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice -= existingItem.price;
+      }
+    },
   },
 });
 
